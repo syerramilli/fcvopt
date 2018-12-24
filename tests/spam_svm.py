698504f76e78860144b83c553033936dec81c1e7
Suraj Yerramilli <surajyerramilli@gmail.com>
2018-12-24 15:21:20 -0600

    modifying a larger test case

--- tests/spam_svm.py
+++ tests/spam_svm.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-
-
 import numpy as np
 import pandas as pd
+import pickle
 from fcvopt.optimizers.fcvopt import FCVOpt
 from sklearn.svm import SVC
 from sklearn.metrics import log_loss
@@ -15,5 +13,11 @@ X = normalize(X)
 
 param_bounds = {'C':[2**-5,2**15],'gamma':[2**-15,2**5]}
 opt = FCVOpt(SVC(probability=True),param_bounds,log_loss,kernel="gaussian",
-             logscale=np.array([0,1]),max_iter=5,seed=2,verbose=2)
+             logscale=np.array([0,1]),max_iter=20,seed=2,verbose=2)
 opt.fit(X,y)
+
+pickle.dump(opt,open('results/spam_svm_gaussian_seed_02.pkl','wb'))
+
+#opt2 = FCVOpt(SVC(probability=True),param_bounds,log_loss,kernel="matern",
+#             logscale=np.array([0,1]),max_iter=20,seed=2,verbose=2)
+#opt2.fit(X,y)
