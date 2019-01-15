7e2bd2b88baa2f92cf37789736bbd282924de7eb
Suraj Yerramilli <surajyerramilli@gmail.com>
2019-01-15 01:44:25 -0600

    reducing default number of restarts

--- fcvopt/util/wrappers.py
+++ fcvopt/util/wrappers.py
@@ -3,14 +3,14 @@
 import numpy as np
 from scipy.optimize import minimize,Bounds
 
-from fcvopt.util.samplers import uniform_sampler
+from fcvopt.util.samplers import lh_sampler
 
-def scipy_minimize(fun,lower,upper,n_restarts=10,maximize=False,rng=None):
-    mstarts = uniform_sampler(n_restarts,lower,upper,rng)
+def scipy_minimize(fun,lower,upper,n_restarts=5,maximize=False,rng=None):
+    mstarts = lh_sampler(n_restarts+1,lower,upper,rng)
     x_best = np.copy(lower)
     f_best = np.Inf
     
-    for i in range(n_restarts):
+    for i in range(n_restarts+1):
         res = minimize(fun,mstarts[i,:],method='L-BFGS-B',
                        bounds=Bounds(lower,upper))
         if res.fun < f_best:
@@ -18,7 +18,3 @@ def scipy_minimize(fun,lower,upper,n_restarts=10,maximize=False,rng=None):
             f_best = res.fun
             
     return x_best,f_best
-        
-              
-    
-        
