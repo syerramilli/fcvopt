0c7288d6900edbd1f2c6ad76bbf784f7d62f3b97
Suraj Yerramilli <surajyerramilli@gmail.com>
2019-01-19 14:02:56 -0600

    fixing numpy warnings

--- fcvopt/util/samplers.py
+++ fcvopt/util/samplers.py
@@ -9,8 +9,8 @@ def uniform_sampler(n_samples,lower,upper,rng=None):
     n_dims = lower.shape[0]
     
     # generate
-    samples = np.stack((rng.uniform(lower[i],upper[i],n_samples) \
-                     for i in range(n_dims)),axis=1)
+    samples = np.stack([rng.uniform(lower[i],upper[i],n_samples) \
+                     for i in np.arange(n_dims)],axis=1)
     return samples
     
 
@@ -21,8 +21,8 @@ def lh_sampler(n_samples,lower,upper,rng=None):
     n_dim = lower.shape[0]
     
     # generate row and column grids
-    grid_bounds = np.stack((np.linspace(lower[i], upper[i], n_samples+1) \
-                            for i in range(2)),axis=1)
+    grid_bounds = np.stack([np.linspace(lower[i], upper[i], n_samples+1) \
+                            for i in np.arange(2)],axis=1)
     grid_lower = grid_bounds[:-1,:]
     grid_upper = grid_bounds[1:,:]
     
