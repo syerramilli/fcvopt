33e8f4f8dccf4f54971d81db4bedf29be8ca2335
Suraj Yerramilli <surajyerramilli@gmail.com>
2019-02-21 17:41:36 -0600

    simplifying code

---
+++ fcvopt/acquisition.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class LCB:
+    def __init__(self,model,kappa=2):
+        self.model = model
+        self.kappa = kappa
+        
+    def update(self,model):
+        self.model = model
+        
+    def __call__(self,x):
+        x_copy = np.copy(x)
+        if x.ndim == 1:
+            x_copy = x_copy.reshape((1,-1))
+        y_mean,y_std = self.model.predict(x_copy,scaled=True,return_std=True)
+        val = y_mean - self.kappa*y_std
+        return val
