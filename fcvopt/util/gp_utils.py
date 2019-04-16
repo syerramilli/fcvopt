25944f0d5f8973186c85d74645628c39b1358fa1
Suraj Yerramilli <surajyerramilli@gmail.com>
2019-04-16 17:54:25 -0500

    Update gp_utils.py

--- fcvopt/util/gp_utils.py
+++ fcvopt/util/gp_utils.py
@@ -1,16 +1,14 @@
 import numpy as np
-from scipy.linalg import cholesky,solve_triangular
+from scipy.linalg import cholesky,cho_solve
 
 def kernel_inv(kernel,X,eps,det=True):
     K = kernel(X)
     K[np.diag_indices_from(K)] += eps*K[0,0]
-    L = cholesky(K, lower=True)
-    
-    L_inv = solve_triangular(L,np.eye(L.shape[0]),lower=True)
-    K_inv = L_inv.T.dot(L_inv)
+    U = cholesky(K,check_finite=False)
+    K_inv = cho_solve((U,False),np.eye(U.shape[0]),check_finite=False)
     
     if det:
-        ldet_K = 2*np.sum(np.log(np.diag(L)))
+        ldet_K = 2*np.sum(np.log(np.diag(U)))
         return K_inv,ldet_K
     else:
         return K_inv
