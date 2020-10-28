c9b68dd0d09a6653a707d0578c1830923c0f653a
Suraj Yerramilli <surajyerramilli@gmail.com>
2020-10-28 15:25:40 -0500

    adding basic hamming kernel

---
+++ fcvopt/kernels/hamming_kernel.py
@@ -0,0 +1,54 @@
+import torch
+from gpytorch.kernels import Kernel
+from gpytorch.constraints import Interval
+
+class HammingKernel(Kernel):
+    has_lengthscale=False
+    def __init__(self,active_dims,**kwargs):
+        super().__init__(has_lengthscale=False,active_dims=active_dims,**kwargs)
+        
+        # register correlation parameter
+        # TODO: add support for multiple indicies
+        self.register_parameter(
+            name='raw_rho',
+            parameter=torch.nn.Parameter(torch.randn(*self.batch_shape,1,1))
+        )
+
+        self.register_constraint('raw_rho',Interval(0.,1.))
+    
+    @property
+    def rho(self):
+        return self.raw_rho_constraint.transform(self.raw_rho)
+    
+    def _set_rho(self,value):
+        raw_value = (
+            self.raw_rho_constraint.
+            inverse_transform(value.to(self.raw_rho))
+        )
+        self.initialize(raw_rho=raw_value)
+    
+    def forward(
+        self,
+        x1,x2,
+        diag=False,
+        last_dim_is_batch=False
+    ):
+        if last_dim_is_batch:
+            x1 = x1.transpose(-1, -2).unsqueeze(-1)
+            x2 = x2.transpose(-1, -2).unsqueeze(-1)
+
+        x1_eq_x2 = torch.equal(x1, x2)
+
+        res = None
+        if diag:
+            # Special case the diagonal because we can return all ones most of the time.
+            if x1_eq_x2:
+                res = torch.ones(*x1.shape[:-2], x1.shape[-2], dtype=x1.dtype, device=x1.device)
+            else:
+                res = 1.-torch.norm(x1,x2,p=0,dim=-1)
+        
+        else:
+            dist = torch.cdist(x1,x2,p=0.)
+            res = 1. - dist.mul(1.-self.rho)
+        
+        return res
