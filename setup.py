91536c1cd509751e81c6fb06d1fbcd5233800954
Suraj Yerramilli <surajyerramilli@gmail.com>
2025-05-11 17:06:46 -0400

    updating version numbers

--- setup.py
+++ setup.py
@@ -12,10 +12,10 @@ with open('exp_requirements.txt') as f:
 
 setup(
       name='fcvopt',
-      version='0.1',
+      version='0.2',
       description='Fractional K-fold cross-validation for hyperparameter optimization',
       url='http://github.com/syerramilli/fcvopt',
-      author='Suraj Yerramilli',
+      author='Suraj Yerramilli, Daniel W. Apley',
       author_email='surajyerramilli@gmail.com',
       license='MIT',
       packages=find_packages(exclude=['notebooks', 'tests', 'examples', 'experiments']),
