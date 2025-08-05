4f1d6fb94657d1a9eb37963bf935070428a5fd3a
Suraj Yerramilli <surajyerramilli@gmail.com>
2025-08-05 15:06:37 -0500

    adding requirements for generating documentation

--- setup.py
+++ setup.py
@@ -12,16 +12,22 @@ with open('exp_requirements.txt') as f:
 
 setup(
       name='fcvopt',
-      version='0.2',
+      version='0.3.0',
       description='Fractional K-fold cross-validation for hyperparameter optimization',
       url='http://github.com/syerramilli/fcvopt',
       author='Suraj Yerramilli, Daniel W. Apley',
       author_email='surajyerramilli@gmail.com',
       license='MIT',
-      packages=find_packages(exclude=['notebooks', 'tests', 'examples', 'experiments']),
+      packages=find_packages(exclude=['notebooks', 'tests', 'experiments']),
       install_requires=install_requires,
       extras_require={
-            'experiments': exp_requirements
+            'experiments': exp_requirements,
+            'docs': [
+                  'sphinx>=4.0',
+                  'sphinx-rtd-theme',
+                  'sphinxcontrib-napoleon',
+                  'sphinx-autodoc-typehints',
+            ]
       },
       zip_safe=False
 )
