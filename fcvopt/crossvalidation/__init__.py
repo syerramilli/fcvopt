7b3aae2ffad88e44b64db0a1e9a80d9f17977f09
Suraj Yerramilli <surajyerramilli@gmail.com>
2026-03-14 18:23:54 -0400

    updated imports

--- fcvopt/crossvalidation/__init__.py
+++ fcvopt/crossvalidation/__init__.py
@@ -1,3 +1,3 @@
 from .cvobjective import CVObjective
-from .sklearn_cvobj import SklearnCVObj, XGBoostCVObjEarlyStopping
+from .sklearn_cvobj import SklearnCVObj
 from .resnet_cvobj import ResNetCVObj
